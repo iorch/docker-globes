20f5ac17e5563ccdc16a5abdab94ac1c52a996eb
i0rch <jorge.martinez@jalisco.gob.mx>
2022-07-20 19:17:13 -0500

    fix bug in file existance check

--- download_globes.sh
+++ download_globes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 FILE=globes-3.2.18.tar.gz
-if [ - "$FILE" ]; then
+if [ -f "$FILE" ]; then
     echo "$FILE existe."
 else
     wget https://www.mpi-hd.mpg.de/personalhomes/globes/download/globes-3.2.18.tar.gz
